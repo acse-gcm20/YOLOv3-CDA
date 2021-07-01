5c14f9e0b1d994a8a0ab323925fb7be52fbb316e
Giles Matthews <giles.matthews@outlook.com>
2021-07-01 20:53:22 +0100

    a

--- pytorchyolo/train.py
+++ pytorchyolo/train.py
@@ -56,7 +56,7 @@ def _create_data_loader(img_path, batch_size, img_size, n_cpu, multiscale_traini
         collate_fn=dataset.collate_fn,
         worker_init_fn=worker_seed_set)
 
-    print(dataloader.__len__())
+    img_path, img, bb_targets = print(dataloader.__getitem__(0))
     return dataloader
 
 
