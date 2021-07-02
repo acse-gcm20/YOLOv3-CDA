f9affbd7bc0e7cf553a7a903c5d6b7fdc2165c13
Giles Matthews <giles.matthews@outlook.com>
2021-07-02 13:01:01 +0100

    more logger flags

--- pytorchyolo/utils/datasets.py
+++ pytorchyolo/utils/datasets.py
@@ -80,7 +80,6 @@ class ListDataset(Dataset):
         self.transform = transform
 
     def __getitem__(self, index):
-        print("Getting Item")
         # ---------
         #  Image
         # ---------
